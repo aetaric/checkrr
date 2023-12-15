9f0ecef3218d9e1b089fc82bd13bfd8a6cbb7b6b
Dustin Essington <aetaric@gmail.com>
2023-12-15 07:54:02 -0700

    enable setting the chatid in the config. always log chatid if it's unset and we have a message from the target username.

--- notifications/telegram.go
+++ notifications/telegram.go
@@ -13,6 +13,7 @@ type Telegram struct {
 	AllowedNotifs []string
 	apiToken      string
 	username      string
+	chatid        int64
 	bot           *tgbotapi.BotAPI
 	Log           log.Logger
 }
@@ -25,20 +26,7 @@ func (t Telegram) Notify(title string, description string, notifType string, pat
 		}
 	}
 	if allowed {
-		var chatid int64
-		updates, err := t.bot.GetUpdates(tgbotapi.UpdateConfig{})
-		if err != nil {
-			return false
-		}
-		for _, update := range updates {
-			t.Log.Debug(fmt.Sprintf("User: %s", update.SentFrom().UserName))
-			if fmt.Sprintf("@%s", update.SentFrom().UserName) == t.username {
-				chatid = update.FromChat().ID
-				t.Log.Debug(fmt.Sprintf("chatid: %v", chatid))
-				break
-			}
-		}
-		message := tgbotapi.NewMessage(chatid, description)
+		message := tgbotapi.NewMessage(t.chatid, description)
 		t.bot.Send(message)
 		return true
 	}
@@ -54,6 +42,20 @@ func (t *Telegram) Connect() bool {
 		t.Log.WithFields(log.Fields{"Error": err, "Username": t.username, "Token": t.apiToken}).Warn("Error connecting to Telegram")
 		return false
 	}
+	updates, err := t.bot.GetUpdates(tgbotapi.UpdateConfig{})
+	if err != nil {
+		return false
+	}
+	if t.chatid == 0 {
+		for _, update := range updates {
+			t.Log.Debug(fmt.Sprintf("User: %s", update.SentFrom().UserName))
+			if fmt.Sprintf("@%s", update.SentFrom().UserName) == t.username {
+				t.chatid = update.FromChat().ID
+				t.Log.Infof("Telegram chatid: %v", t.chatid)
+				break
+			}
+		}
+	}
 	t.Log.Info("Connected to Telegram")
 	return true
 }
@@ -62,5 +64,6 @@ func (t *Telegram) FromConfig(config viper.Viper) {
 	t.config = config
 	t.apiToken = config.GetString("apitoken")
 	t.username = config.GetString("username")
+	t.chatid = config.GetInt64("chatid")
 	t.AllowedNotifs = config.GetStringSlice("notificationtypes")
 }
