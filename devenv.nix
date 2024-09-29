2f8ff8cbd4b3e5e1d81db35f7362b116d995770d
aetaric <aetaric@aetaric.ninja>
2024-09-29 15:16:10 -0600

    add devenv for quick development on nixos

---
+++ devenv.nix
@@ -0,0 +1,43 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ 
+    pkgs.git
+    pkgs.go
+    pkgs.nodejs
+    pkgs.yarn
+  ];
+
+  languages = {
+    go.enable = true;
+    javascript = {
+      enable = true;
+      npm = { 
+        enable = true;
+        install.enable = true;
+      };
+      yarn = {
+        enable = true;
+        install.enable = true;
+      };
+    };
+  };
+
+  enterShell = ''
+    go version
+    node --version
+    yarn --version
+  '';
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
