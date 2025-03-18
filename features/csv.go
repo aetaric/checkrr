49c4f7d3f2373b5a75e064702bc829ce9147006d
aetaric <565454+aetaric@users.noreply.github.com>
2025-03-18 14:38:49 -0600

    fixed csv writter

--- features/csv.go
+++ features/csv.go
@@ -2,9 +2,10 @@ package features
 
 import (
 	"encoding/csv"
+	"os"
+
 	"github.com/aetaric/checkrr/logging"
 	"github.com/nicksnyder/go-i18n/v2/i18n"
-	"os"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -19,7 +20,7 @@ type CSV struct {
 
 func (c *CSV) Open() {
 	var err error
-	c.fileHandle, err = os.Create(c.FilePath)
+	c.fileHandle, err = os.OpenFile(c.FilePath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0660)
 	if err != nil {
 		message := c.Localizer.MustLocalize(&i18n.LocalizeConfig{
 			MessageID: "CSVFileCreateFailed",
@@ -29,11 +30,18 @@ func (c *CSV) Open() {
 		})
 		c.Log.WithFields(log.Fields{"startup": true}).Fatalf(message)
 	}
-	defer c.fileHandle.Close()
 	c.fileWriter = csv.NewWriter(c.fileHandle)
-	defer c.fileWriter.Flush()
 }
 
 func (c *CSV) Write(path string, t string) {
 	c.fileWriter.Write([]string{path, t})
+	c.fileWriter.Flush()
+	c.Log.Debug("wrote csv entry")
+}
+
+func (c *CSV) Close() {
+	c.fileWriter.Flush()
+	c.fileHandle.Sync()
+	c.fileHandle.Close()
+	c.Log.Debug("closed csv file")
 }
